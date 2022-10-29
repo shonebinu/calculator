c9cb7a8158eb97d513a9c2843f64a37f7067db23
Shone Binu <shonebinualias@gmail.com>
2022-10-29 10:27:18 +0530

    Added the basic functions for basic arithematic

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+//add
+function add(a, b) {
+    return a+b;
+}
+
+//subtract
+function subtract(a, b) {
+    return a-b;
+}
+
+//multiply
+function multiply(a, b) {
+    return a*b;
+}
+
+//divide
+function divide(a, b) {
+    return a/b;
+}
