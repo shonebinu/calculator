d5ad49ec6c3fe8443f8b385acc0e6e7597e5833c
Shone Binu <shonebinualias@gmail.com>
2022-10-29 10:34:10 +0530

    Operate the inputs based on the operator

--- index.js
+++ index.js
@@ -19,3 +19,26 @@ function multiply(a, b) {
 function divide(a, b) {
     return a/b;
 }
+
+
+//operate the inputs based on the operator
+const operate = (operator, a, b) => {
+    switch (operator) {
+
+        case '+':
+            return add(a, b);
+            break;
+
+        case '-':
+            return subtract(a, b);
+            break;       
+
+        case '*':
+            return multiply(a, b);
+            break;
+    
+        case '/':
+            return divide(a, b);
+            break;           
+        }
+}
