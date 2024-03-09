702685a59fd00ddbc240d6dc016a60e5d4333832
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2024-03-09 16:37:45 +0530

    feat: basic implementation of calculator page

---
+++ scripts.js
@@ -0,0 +1,20 @@
+"use strict";
+
+function Calculator() {
+  this.methods = {
+    "+": (a, b) => a + b,
+    "-": (a, b) => a - b,
+    "*": (a, b) => a * b,
+    "/": (a, b) => a / b,
+  };
+
+  this.operate = function(a, op, b) {
+    if (!this.methods[op] || isNaN(a) || isNaN(b)) {
+      return NaN;
+    }
+
+    return this.methods[op](a, b);
+  };
+}
+
+const calculator = new Calculator();
