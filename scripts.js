957f19f78f9ea43e7cac77d65adbb1dee4799fa6
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2024-03-10 09:44:04 +0530

    feat: add basic binary operations

--- scripts.js
+++ scripts.js
@@ -18,3 +18,43 @@ function Calculator() {
 }
 
 const calculator = new Calculator();
+
+let displayValue = "";
+const display = document.querySelector(".display");
+const numButtons = document.querySelectorAll(".num");
+const opButtons = document.querySelectorAll(".op");
+const equalButton = document.querySelector("[data-equal]");
+
+numButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    if ("+-*/".includes(displayValue.at(-1))) {
+      displayValue += " ";
+    }
+    displayValue += button.textContent;
+    display.textContent = displayValue;
+  });
+});
+
+opButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    if ("+-*/".includes(displayValue.at(-1))) {
+      return;
+    }
+    if ("0123456789".includes(displayValue.at(-1))) {
+      displayValue += " ";
+    }
+    displayValue += button.textContent;
+    display.textContent = displayValue;
+  });
+});
+
+equalButton.addEventListener("click", () => {
+  let a, op, b;
+  [a, op, b] = displayValue.split(" ");
+  [a, b] = [+a, +b];
+
+  let result = calculator.operate(a, op, b);
+  displayValue = `${result}`;
+
+  display.textContent = displayValue;
+});
